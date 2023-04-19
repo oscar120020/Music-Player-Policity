38bbdedbfcd73a18121c3a272ca9198673124485
oscar120020 <oscarmiguel12002@gmail.com>
2023-04-18 21:38:03 -0400

    politica de privacidad

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Política de privacidad de la aplicación Music Player</h1>
+
+      <h2>Fecha de entrada en vigor: 04/18/2023</h2>
+
+      <p>
+        La aplicación Music Player ha sido creada por om12002, con el propósito
+        de permitir a los usuarios reproducir música en sus dispositivos
+        móviles. La aplicación está disponible para su descarga en la plataforma
+        Google Play Store y es compatible con dispositivos Android.
+      </p>
+
+      <p>
+        Esta política de privacidad describe cómo recopilamos, usamos y
+        compartimos la información de los usuarios que utilizan la aplicación.
+        Al utilizar la aplicación, acepta la recopilación y el uso de su
+        información de acuerdo con esta política de privacidad. Si no está de
+        acuerdo con alguna parte de esta política de privacidad, no utilice la
+        aplicación.
+      </p>
+
+      <h2>Recopilación de información</h2>
+
+      <p>
+        La aplicación recopila información de los usuarios de las siguientes
+        maneras:
+      </p>
+
+      <p>
+        Permisos de acceso a archivos multimedia: La aplicación requiere acceso
+        a los archivos multimedia del dispositivo para poder reproducir la
+        música seleccionada por el usuario. Al otorgar los permisos necesarios,
+        la aplicación podrá acceder a los archivos multimedia del dispositivo y
+        mostrarlos en la interfaz de usuario de la aplicación.
+      </p>
+
+      <p>
+        Registros de uso: La aplicación registra información sobre el uso de la
+        aplicación, como la frecuencia con la que se utiliza, las canciones que
+        se reproducen con más frecuencia y los errores que se producen durante
+        la reproducción.
+      </p>
+
+      <h2>Uso de la información</h2>
+
+      <p>
+        La información recopilada por la aplicación se utiliza para los
+        siguientes fines:
+      </p>
+
+      <p>
+        Proporcionar servicios de reproducción de música: La aplicación utiliza
+        los permisos de acceso a archivos multimedia para permitir a los
+        usuarios reproducir música desde su dispositivo móvil.
+      </p>
+
+      <p>
+        Mejorar la aplicación: La información recopilada sobre el uso de la
+        aplicación se utiliza para mejorar la calidad y el rendimiento de la
+        aplicación, corregir errores y agregar nuevas características.
+      </p>
+
+      <h2>Compartir información</h2>
+
+      <p>
+        La información recopilada por la aplicación no se compartirá con
+        terceros sin el consentimiento previo del usuario.
+      </p>
+
+      <h2>Seguridad de la información</h2>
+
+      <p>
+        La aplicación tomará las medidas razonables para proteger la información
+        del usuario de la pérdida, el uso indebido y el acceso no autorizado.
+        Sin embargo, debido a la naturaleza de Internet, no podemos garantizar
+        la seguridad de la información transmitida a través de la red.
+      </p>
+
+      <h2>Cambios en la política de privacidad</h2>
+
+      <p>
+        La aplicación puede modificar esta política de privacidad de vez en
+        cuando. Si se realizan cambios significativos en la forma en que se
+        utiliza la información del usuario, se notificará a los usuarios
+        mediante una actualización de la política de privacidad en la
+        aplicación.
+      </p>
+
+      <h2>Contacto</h2>
+
+      <p>
+        Si tiene preguntas o inquietudes sobre esta política de privacidad,
+        puede contactar a om12002 a través de la siguiente dirección de correo
+        electrónico: oscarmiguel12002@gmail.com.
+      </p>
+
+      <h2>Aceptación de la política de privacidad</h2>
+
+      <p>
+        Al utilizar la aplicación, usted acepta esta política de privacidad. Si
+        no está de acuerdo con esta política de privacidad, por favor desinstale
+        la aplicación.
+      </p>
+    </div>
+  );
+}
+
+export default App;
